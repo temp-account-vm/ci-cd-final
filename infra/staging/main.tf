ddfb9b981ce464fd24c6e7936ec05ca44c7d0bc0
Quentin-Su <Quentin.Sussetto@outlook.fr>
2025-06-10 10:13:17 +0200

    feat: terraform with stage

---
+++ infra/staging/main.tf
@@ -0,0 +1,45 @@
+provider "google" {
+  credentials = file("../credentials.json")
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "staging-terraform-network"
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "staging-allow-ssh-api"
+  network = google_compute_network.vpc_network.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "3000", "8080"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance" "staging" {
+  name         = "staging-api-instance"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {}
+  }
+
+    metadata = {
+    ssh-keys = "debian:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  metadata_startup_script = "sudo apt update && sudo apt install -y git"
+}
