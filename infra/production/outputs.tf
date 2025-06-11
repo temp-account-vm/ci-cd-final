d9be9ef1823891220c05a2530d7b343a10dc45ea
Quentin-Su <Quentin.Sussetto@outlook.fr>
2025-06-11 22:36:24 +0200

    fix: snapshots & rollback disk

--- infra/production/outputs.tf
+++ infra/production/outputs.tf
@@ -1,3 +1,7 @@
 output "instance_ip" {
   value = google_compute_instance.production.network_interface[0].access_config[0].nat_ip
 }
+
+output "boot_disk" {
+  value = google_compute_instance.production.boot_disk[0].device_name
+}
--- infra/staging/outputs.tf
+++ infra/staging/outputs.tf
@@ -1,3 +1,7 @@
 output "instance_ip" {
   value = google_compute_instance.staging.network_interface[0].access_config[0].nat_ip
 }
+
+output "boot_disk" {
+  value = google_compute_instance.staging.boot_disk[0].device_name
+}
