2bf1c7fcb71c940c042fe10cdbcae8d8460c18ed
Quentin-Su <Quentin.Sussetto@outlook.fr>
2025-06-12 01:38:45 +0200

    fix: terraform output disk name

--- infra/production/outputs.tf
+++ infra/production/outputs.tf
@@ -2,6 +2,6 @@ output "instance_ip" {
   value = google_compute_instance.production.network_interface[0].access_config[0].nat_ip
 }
 
-output "boot_disk" {
+output "boot_disk_name" {
   value = google_compute_instance.production.boot_disk[0].device_name
 }
--- infra/staging/outputs.tf
+++ infra/staging/outputs.tf
@@ -2,6 +2,6 @@ output "instance_ip" {
   value = google_compute_instance.staging.network_interface[0].access_config[0].nat_ip
 }
 
-output "boot_disk" {
+output "boot_disk_name" {
   value = google_compute_instance.staging.boot_disk[0].device_name
 }
